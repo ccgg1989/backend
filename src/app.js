2165da9aa2a79e15572e0b7275441e0845ceabe2
nivel99 <inteligenciafuturatv@gmail.com>
2024-12-08 20:07:52 -0500

    se agrega lo de la carpeta src

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import morgan from 'morgan'
+import morgan from 'morgan';
 import cors from 'cors';
+import path from 'path'; // Importar path para manejar rutas
 import logger from './logger.js';
-import {createRoles} from  './libs/initialSstup.js'
-
+import { createRoles } from './libs/initialSstup.js';
 
 import RouterAuth from './routes/auth.routes.js';
 import RouterUser from './routes/user.routes.js';
@@ -11,43 +11,29 @@ import RouterProducts from './routes/product.routes.js';
 import RouterSuggested from './routes/suggested.routes.js';
 import RouterCookies from './routes/cookies.routes.js';
 
-const app = express()
+const app = express();
 
+// Middlewares
 app.use(morgan('dev'));
 app.use(express.text());
 app.use(express.json());
-createRoles()
+createRoles();
 
+// Servir archivos estáticos desde la carpeta uploads
+app.use('/uploads', express.static(path.resolve('uploads')));
 
-/*const whitelist = ['http://127.0.0.1:5173'];
-
-const corsOptions = {
-    origin: function(origin, callback){
-        console.log(origin)
-        if(whitelist.includes(origin)) {
-           //the query la API it`s successfully
-           callback(null, true)
-        }else {
-            //the query it`s not allow
-            callback(new Error("Cors`s Errors"))
-        }
-    },
-
-}
-
-app.use(cors(corsOptions))*/
-
-app.get('/', function(req, res) {
-    logger.info("ok")
+// Rutas
+app.get('/', (req, res) => {
+    logger.info('ok');
     res.json({
-                message: "The Farmark"
-      });
+        message: 'The Farmark',
     });
+});
 
-app.use('/api/auth', RouterAuth)
-app.use('/api/user', RouterUser)
-app.use('/api/products', RouterProducts)
-app.use('/api/suggested', RouterSuggested)
-app.use('/api/cookies', RouterCookies)
+app.use('/api/auth', RouterAuth);
+app.use('/api/user', RouterUser);
+app.use('/api/products', RouterProducts);
+app.use('/api/suggested', RouterSuggested);
+app.use('/api/cookies', RouterCookies);
 
 export default app;
